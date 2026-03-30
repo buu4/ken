9bf60065b734a0ad127bd86305b5ef51c17aae57
buu4 <stakk.github@gmail.com>
2026-03-30 16:57:09 +0700

    Add hashmap implementation for faster mapping
    Because programming language is mostly doing thousand of keyword or scope check
    This will be usefull for optimized check in the future

--- kenc.h
+++ kenc.h
@@ -91,4 +91,28 @@ Token *lex_next(Lexer *l);
 Token *lex_tokenize(Lexer *l, int *count);
 const char *token_type_name(TokenType type);
 
+//
+// hashmap.c
+//
+
+typedef struct {
+  char *key;
+  int keylen;
+  void *val;
+} HashEntry;
+
+typedef struct {
+  HashEntry *buckets;
+  int capacity;
+  int used;
+} HashMap;
+
+void *hashmap_get(HashMap *map, char *key);
+void *hashmap_get2(HashMap *map, char *key, int keylen);
+void hashmap_put(HashMap *map, char *key, void *val);
+void hashmap_put2(HashMap *map, char *key, int keylen, void *val);
+void hashmap_delete(HashMap *map, char *key);
+void hashmap_delete2(HashMap *map, char *key, int keylen);
+void hashmap_test(void);
+
 #endif
